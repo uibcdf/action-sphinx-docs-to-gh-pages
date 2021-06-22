4bee13e8739a139d888477f35fb2c7ca42d5345e
Diego Prada <prada.gracia@gmail.com>
2021-06-21 20:09:10 -0500

    Fixing work with env variables

--- gh-pages_branch.sh
+++ gh-pages_branch.sh
@@ -5,8 +5,6 @@ echo $COMMITTER_NAME
 
 if [[ -z $(git ls-remote --heads origin gh-pages) ]]; then
    echo "Creating gh-pages branch"
-   git config --global user.name "$COMMITTER_NAME"
-   git config --global user.email "$COMMITTER_EMAIL"
    git checkout --orphan gh-pages
    git reset --hard
    git commit --allow-empty -m "First commit to create gh-pages branch"
